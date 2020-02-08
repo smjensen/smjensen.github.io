0bacd137b20e2db4211cb63945859286d7fbede7
smjensen <jen15097@byui.edu>
2020-02-08 15:57:22 -0700

    fixing date

--- js/home.js
+++ js/home.js
@@ -25,3 +25,23 @@ let updateTime = updateMonth + "/" + updateDay + "/" + lastUpdate.getFullYear() 
 document.getElementById("currentyear").innerHTML = year;
 
 document.getElementById("lastUpdateTime").innerHTML = updateTime;
+
+//const options={minute: "numeric", hour: "numeric", day: "numeric", month: "long", }
+const options= {weekday: "long", day: "numeric", month: "long", year:"numeric"};
+          
+document.getElementById('currentDate').textContent=new Date().toLocaleDateString("en-GB", options);
+// const modoptions ={ second: "numeric", minute: "numeric", hour: "numeric", day: "numeric", month:"numeric", year: "numeric"};
+//document.getElementById('lastModified').textContent=new Date(document.lastModified);
+//document.getElementById('lastModified').textContent=document.lastModified;
+function toggleMenu() {
+   // console.log(document.getElementById("topNav").classList);
+    document.getElementById("topNav").classList.toggle("hide");
+}
+function showBanner() {
+    var today= new Date();
+    var wkday= today.getDay();
+    if (wkday == 5){
+    document.getElementById("banner").classList.toggle("noshow");
+   
+}
+}
