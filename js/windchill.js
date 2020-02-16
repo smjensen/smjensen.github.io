b2f890ebd0d32651fdfc404aabad9c379137e342
smjensen <jen15097@byui.edu>
2020-02-15 22:57:43 -0700

    fixing the windchill temp

--- js/windchill.js
+++ js/windchill.js
@@ -1,14 +1,13 @@
-let t = document.getElementById('temp').innerHTML;
-let s = document.getElementById('windSpeed').innerHTML;
-
-if ((t <= 50) && (s >= 3)) {
-
-let f = (35.74+0.6215*t) - (35.75*Math.pow(s,0.16)) +0.4275*t*(Math.pow(s,0.16));
- 
-
-document.getElementById('windChill').innerHTML = Math.round(f);
-}
-
-else {
-document.getElementById('windChill').innerHTML = 'N/A';
-};
+    
+    let t = parseFloat(document.getElementById("temp").textContent);
+    let s = parseFloat(document.getElementById("windspeed").textContent);
+    
+    let f;
+    if (t<=50 && s>=3.0) {
+    
+    f = 35.74 + 0.6215*t - 35.75*s**0.16 + 0.4275*t*s**0.16;
+    f= f.toFixed(0);
+    }
+    else {f= "N/A";}
+   
+    document.getElementById("windchill").textContent = f;
