060010c88e4b5bd738035466770def99402a7333
smjensen <jen15097@byui.edu>
2020-02-21 19:10:05 -0700

    adding new

--- js/observer.js
+++ js/observer.js
@@ -1,34 +1,57 @@
-//get all imgs with data-src attribute
-let imagesToLoad= document.querySelectorAll('img[data-src]');
-
-//optional parameters being set for the IntersectionalObserver
-/*const imgOptions= {
-    threshold: 0,
-    rootMargin: "0px 0px -100px 0px"
-};*/
-
-const loadImages= (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload= () => {image.removeAttribute('data-src');};
-};
-
-//first check to see if Intersection Observer is supported
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+}; 
 if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer)=> {
-        items.forEach((item) => {
-            if (item.isIntersecting) {
-                loadImages(item.target);
-                observer.unobserve(item.target);
-            }
-        });
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
     });
     imagesToLoad.forEach((img) => {
-        observer.observe(img);
+      observer.observe(img);
     });
-} else {
 
-    imagesToLoad.forEach((img)=> {
-        loadImages(img);
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
     });
-        
+  }
+
+/*const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+    const src= img.getAttribute("data-src");
+    if (!src){
+        return;
+    }
+    img.src = src;
 }
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -50px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, 
+    imgObserver) => {
+  entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+          return;
+      } else {
+          preloadImage(entry.target);
+          imgObserver.unobserve(entry.target);
+      }
+  })
+      
+},imgOptions);
+
+ images.forEach(image => {
+     imgObserver.observe(image);
+ }); */
