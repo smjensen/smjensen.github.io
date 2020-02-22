98a6b108895aeb577769380782cc89fac0a0fa97
smjensen <jen15097@byui.edu>
2020-02-21 18:22:26 -0700

    fixing

---
+++ js/observer.js
@@ -0,0 +1,34 @@
+//get all imgs with data-src attribute
+let imagesToLoad= document.querySelectorAll('img[data-src]');
+
+//optional parameters being set for the IntersectionalObserver
+/*const imgOptions= {
+    threshold: 0,
+    rootMargin: "0px 0px -100px 0px"
+};*/
+
+const loadImages= (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload= () => {image.removeAttribute('data-src');};
+};
+
+//first check to see if Intersection Observer is supported
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer)=> {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+
+    imagesToLoad.forEach((img)=> {
+        loadImages(img);
+    });
+        
+}
