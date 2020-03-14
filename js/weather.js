29a6466771dd46bf1f22f38ff9e918e48c5c6aeb
smjensen <jen15097@byui.edu>
2020-03-14 00:42:40 -0600

    fix

--- js/weather.js
+++ js/weather.js
@@ -56,3 +56,34 @@ fetch(apiURL)
    
      
     });
+
+   /*==================windchill===============================*/ 
+    let t = parseFloat(document.getElementById("temp").textContent);
+    let s = parseFloat(document.getElementById("windspeed").textContent);
+    
+    let f;
+    if (t<=50 && s>=3.0) {
+    
+    f = 35.74 + 0.6215*t - 35.75*s**0.16 + 0.4275*t*s**0.16;
+    f= f.toFixed(0) + "&#8457;";
+    }
+    else {f= "N/A";}
+   
+    document.getElementById("windchill").textContent = f; 
+
+/*//works with <body onload="windChill()">
+function windChill() {
+    
+    let t = parseFloat(document.getElementById("temp").textContent);
+    let s = parseFloat(document.getElementById("windspeed").textContent);
+    //console.log(s);
+    let f;
+    if (t<=50 && s>=3.0) {
+    
+    f = 35.74 + 0.6215*t - 35.75*s**0.16 + 0.4275*t*s**0.16;
+    f= f.toFixed(0)+ "&#8457;";
+    }
+    else {f= "N/A";}
+   
+    document.getElementById("windchill").textContent = f;
+}*/
