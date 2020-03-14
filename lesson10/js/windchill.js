a20ead169a9239233aa4718d456447fb8e34654c
smjensen <jen15097@byui.edu>
2020-03-14 13:07:33 -0600

    new weather

--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -1,35 +1,89 @@
-//----------------- Wind Chill Calculator -----------------
 
-const tempF = parseFloat(document.getElementById("valHigh").textContent);
-const speed = parseFloat(document.getElementById("valWind").textContent);
+const d= new Date();
+const todayDayNum= d.getDay();
+const weekday= [];
+weekday[0] ="Sun";
+weekday[1] ="Mon";
+weekday[2] ="Tue";
+weekday[3] ="Wed";
+weekday[4] = "Thu";
+weekday[5] ="Fri";
+weekday[6] ="Sat";
 
-/* Celsius Calculator
+const apiURL="https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=e7e99f72ef4dc80d87dfcaa6b6420751";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    //console.log(jsObject);
+    document.getElementById('current').textContent = jsObject.list[0].weather[0].main;
+    document.getElementById('temp').textContent = jsObject.list[0].main.temp_max.toFixed(0);
+    document.getElementById('windspeed').textContent = jsObject.list[0].wind.speed.toFixed(0);
+    document.getElementById('humidity').textContent = jsObject.list[0].main.humidity;
+    
+    
+   const time = jsObject.list;
+   let dtemp= 1;
+   let forecastDayNum=todayDayNum;
+   for (let i=0; i<time.length; i++) {
+    
+    if (time[i].dt_txt.includes("18:00:00")) {
+      
+       
+      let icon = time[i].weather[0].icon;
+   let descr= time[i].weather[0].description;
+   let iconsrc= 'https://openweathermap.org/img/w/' + icon + '.png';
+   let daytemp = jsObject.list[i].main.temp.toFixed(0);
 
-if(tempC < 11 && speed < 4){
-var tempF = (tempC * 9 /5) + 32; // Convert Celsius to Fahrenheit
-var chill = windChill(tempF, speed); // Calculate Wind chill
-var chillC = (chill - 32) * 5/9; // Convert back to Celsius
-document.getElementById('valChill').textContent = chillC.toFixed(1);
-}
-else {
-	document.getElementById('valChill').textContent = "N/A";
-	document.getElementById('windUnit').textContent = "";
-} */
+   
+   let mytemp="daytemp" + dtemp;
+   let myicon= "icon" + dtemp;
+   document.getElementById(mytemp).innerHTML= daytemp + "&#8457;";
+   document.getElementById(myicon).setAttribute('src', iconsrc);
+   document.getElementById(myicon).setAttribute('alt', descr);
+
+   forecastDayNum+=1;
+    if(forecastDayNum===7) {
+      forecastDayNum=0;}
+     
+      let myweekday= "weekday" +dtemp;
+      document.getElementById(myweekday).innerHTML=weekday[forecastDayNum];
+
+   dtemp+=1;
+   
 
 
-// Fahrenheit calculator
+     }}
+   
+     
+    });
 
-if (tempF < 51 && speed < 4){
-	var chill = windChill(tempF, speed);
-	document.getElementById('valChill').textContent = chill.toFixed(1); 
-}
-else {
-	document.getElementById('valChill').textContent = "N/A";
-	document.getElementById('windUnit').textContent = "";
-}
+   /*==================windchill===============================*/ 
+    let t = parseFloat(document.getElementById("temp").textContent);
+    let s = parseFloat(document.getElementById("windspeed").textContent);
+    
+    let f;
+    if (t<=50 && s>=3.0) {
+    
+    f = 35.74 + 0.6215*t - 35.75*s**0.16 + 0.4275*t*s**0.16;
+    f= f.toFixed(0) + "&#8457;";
+    }
+    else {f= "N/A";}
+   
+    document.getElementById("windchill").textContent = f; 
 
-function windChill (TempF, speed) {
-	var speedPower = Math.pow(speed, 0.16);
-	var windChillFactor = 35.74 + (0.6215 * TempF) - (35.75 * speedPower) + (0.4275 * TempF * speedPower);
-	return windChillFactor;
-}
+/*//works with <body onload="windChill()">
+function windChill() {
+    
+    let t = parseFloat(document.getElementById("temp").textContent);
+    let s = parseFloat(document.getElementById("windspeed").textContent);
+    //console.log(s);
+    let f;
+    if (t<=50 && s>=3.0) {
+    
+    f = 35.74 + 0.6215*t - 35.75*s**0.16 + 0.4275*t*s**0.16;
+    f= f.toFixed(0)+ "&#8457;";
+    }
+    else {f= "N/A";}
+   
+    document.getElementById("windchill").textContent = f;
+}*/
